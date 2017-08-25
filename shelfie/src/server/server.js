efb45323ac6000c123c91f2c5b3121a23628d755
Daniel Valentine <mrdanvalentine@gmail.com>
2017-08-25 11:54:49 -0600

    added basic file structure

--- shelfie/src/server/server.js
+++ shelfie/src/server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
 const connectionString = 'postgres://postgres:password@localhost/Shelfie';
-const products_controller = require('./products_controller');
+const products_controller = require('./binCtrl');
 
 const app = express();
 app.use( bodyParser.json() );
@@ -13,12 +13,12 @@ massive( connectionString ).then(
 	dbInstance => {
 	console.log("im connected");
 	app.set('db', dbInstance) 
-	});
-app.post( '/api/product', products_controller.create );
-app.get( '/api/products', products_controller.getAll );
-app.get( '/api/product/:id', products_controller.getOne );
-app.put( '/api/product/:id', products_controller.update );
-app.delete( '/api/product/:id', products_controller.delete );
+    });
+    
+app.get( '/api/:shelfId/bins', products_controller.getAll );
+app.get( '/api/bin/:id', products_controller.getOne );
+app.put( '/api/bin/:id', products_controller.update );
 
-const port = 3000;
+
+const port = 3030;
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
